727b5578d8f45f5a443ac832754c1534153fa970
fajaramaulana <fajaragusmaulana@gmail.com>
2022-09-08 15:23:10 +0700

    install godotenv

--- config/database.go
+++ config/database.go
@@ -2,13 +2,19 @@ package config
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectionDb() *gorm.DB {
-	dsn := "root:root@tcp(127.0.0.1:3306)/go-startup?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := os.Getenv("MYSQL")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
