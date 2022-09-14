f176529f12ed20956ed915d0d6fa0da35c38c77b
fajaramaulana <fajaragusmaulana@gmail.com>
2022-09-14 15:17:29 +0700

    install go-jwt dan service auth generatejwt

---
+++ auth/service.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"log"
+	"os"
+
+	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/joho/godotenv"
+)
+
+type Service interface {
+	GenerateToken(userID int, levelUser int) (string, error)
+}
+
+type jwtService struct {
+}
+
+func getSecretKeyJWT() []byte {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dsn := os.Getenv("JWT_SECRET_KEY")
+
+	return []byte(dsn)
+}
+
+func NewService() *jwtService {
+	return &jwtService{}
+}
+
+func (s *jwtService) GenerateToken(userID int, levelUser int) (string, error) {
+
+	payload := jwt.MapClaims{}
+
+	payload["userId"] = userID
+	payload["levelUser"] = levelUser
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
+
+	secretKey := getSecretKeyJWT()
+	signetToken, err := token.SignedString(secretKey)
+
+	if err != nil {
+		return signetToken, err
+	}
+
+	return signetToken, nil
+}
